117d9d5b5045ff3ee6766207986d63cc8c85108f
NuRifat <najimullahrifat@gmail.com>
2026-08-25 15:04:04 +0600

    Backend & frontend connect through cors-headers

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
