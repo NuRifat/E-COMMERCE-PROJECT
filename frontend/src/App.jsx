80ce95450aafefb5c6370e1f24c6f99a9481731e
NuRifat <najimullahrifat@gmail.com>
2026-08-29 15:55:03 +0600

    L-6 Cart Functionality (Frontend)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import ProductList from './pages/ProductList';
 import ProductDetails from './pages/ProductDetails';
+import CartPage from './pages/CartPage';
+import Navbar from './components/Navbar';
 
 function App() {
   
   return (
     <Router>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<ProductList/>} />
         <Route path="/product/:id" element={<ProductDetails/>} />
+        <Route path="/cart" element={<CartPage/>} />
       </Routes>
     </Router>
   );
